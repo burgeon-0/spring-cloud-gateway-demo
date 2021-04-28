dd51b50952838a12026cb0ac64d1e77300c3f5fc
Sam Lu <luxiaocong524@163.com>
2021-04-28 19:54:43 +0800

    first commit

---
+++ src/main/java/org/burgeon/scgd/Application.java
@@ -0,0 +1,19 @@
+package org.burgeon.scgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Demo project for Spring Cloud Gateway
+ *
+ * @author Sam Lu
+ * @date 2021/4/28
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/burgeon/scgd/ApplicationTests.java
@@ -0,0 +1,17 @@
+package org.burgeon.scgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Sam Lu
+ * @date 2021/4/28
+ */
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
